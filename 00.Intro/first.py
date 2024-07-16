754cb3fa4fe94aa5c9a2bc958a8de439b8d9c738
Mayank Vishnoi <mayankvishnoi24@gmail.com>
2024-07-16 12:01:29 +0530

    README

---
+++ 00.Intro/first.py
@@ -0,0 +1,10 @@
+# print("Hii") #print() is a function. print is used for the output print on the display..
+
+
+# Python Data is Defining the more Informative like this 👇👇
+print("Addition Result 5 + 5 = ",5+5)
+print("Subraction Result 5 - 5 = ",5-5)
+print("Multiply Result 5 * 5 = ",5*5)
+print("Division Result 5 / 5 = ",5/5)
+
+# 4+5 this line off code is executed.. 
---
+++ 00.Intro/second.py
@@ -0,0 +1,5 @@
+#  Comment in python
+
+        # This is a comment line. Its not read the machine and don't print
+        # Shortcut key :  ctrl+/
+        # This is a first program creating the programming notes.
