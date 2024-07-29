8f5b110d22b5a24553a3815870e45c450e92947c
Mayank Vishnoi <mayankvishnoi24@gmail.com>
2024-07-29 15:29:36 +0530

    Create sixty.py

---
+++ 02.Datatypes(non-primitive)/sixty.py
@@ -0,0 +1,18 @@
+# Dictionary Datatype
+
+# # Indentification Dictionary {} courly Bracket
+
+# Example:   
+#             {   'a'   :   'apple'   }
+#                 Key        # Value
+
+#             {   'a'   :   ['apple','ant','anaconda']}
+#                             Multiple values in a dictionary
+
+# dic = {'a':'apple'}
+# # print(dic)
+# print(dic['a'])
+# print
+
+dic = {'a':'apple','b':'banana'}
+print(dic)
