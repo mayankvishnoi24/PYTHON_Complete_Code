1504209810f693bc5c3bae8afa49f5d3280a1b50
Mayank Vishnoi <mayankvishnoi24@gmail.com>
2024-07-29 15:39:20 +0530

    README.md

--- 02.Datatypes(non-primitive)/sixty.py
+++ 02.Datatypes(non-primitive)/sixty.py
@@ -9,10 +9,16 @@
 #             {   'a'   :   ['apple','ant','anaconda']}
 #                             Multiple values in a dictionary
 
-# dic = {'a':'apple'}
-# # print(dic)
+dic = {'a':['apple','ant','anaconda'],1:200,False:0}
+# print(dic)
 # print(dic['a'])
-# print
+# print(dic[1])
+# print(dic[False])
+# dic[1]= 300
+# dic['b'] = 'ball'
+print(dic['a'][2])
 
-dic = {'a':'apple','b':'banana'}
-print(dic)
+
+
+# dic = {'a':'apple','b':'ball'}
+# print(dic)
