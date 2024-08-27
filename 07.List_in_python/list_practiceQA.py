c3aa5749c8bd692259f123f5b9b5a963e34f38a6
Mayank Vishnoi <134063120+mayankvishnoi24@users.noreply.github.com>
2024-08-27 17:56:43 +0530

    Updated

---
+++ 07.List_in_python/list_practiceQA.py
@@ -0,0 +1,88 @@
+# 1. Write a Python program to sum all the items in a list
+
+# l = [1,2,3,4,5,6,7,8,9,10]
+# s = 0
+# for i in l:
+#     s = s + i
+# print("Sum",s)
+# print(sum(l))
+
+
+# 2. Write a Python program to multiplies all the items in a list. 
+
+# m = [1,2,3,4,5,6,7,8,9,10]
+# s = 1
+# for i in m:
+#     s = s * i
+# print("Multiplication",s)
+
+
+# 3. Write a Python program to get the largest number from a list. 
+
+# 4. Write a Python program to get the smallest number from a list. 
+
+# 5. Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings. 
+# Sample List : ['abc', 'xyz', 'aba', '1221']
+# Expected Result : 2
+
+# l = ['abc', 'xyz', 'aba', '1221']
+# count = 0
+# for i in l:
+#     if len(i)>=2 and i[0]==i[-1]:
+#         count += 1
+# print(count)
+
+
+# 6. Write a Python program to remove duplicates from a list. 
+
+# 7. Write a Python program to check a list is empty or not. 
+
+# 8. Write a Python program to clone or copy a list. 
+
+# 9. Write a Python program to find the list of words that are longer than n from a given list of words. 
+
+# 10. Write a Python program that takes two lists and returns True if they have at least one common member. 
+
+# 11. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements. 
+# Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+# Expected Output : ['Green', 'White', 'Black']
+
+# l = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+# l.pop(5)
+# print(l)
+# l.pop(4)
+# print(l)
+# l.pop(0)
+# print(l)
+
+
+# 12. Write a Python program to generate a 3*4*6 3D array whose each element is *. 
+
+# l3 = []
+# for k in range(3):
+#     l2 = []
+#     for j in range(4):
+#         l1 = []
+#         for i in range(6):
+#             l3.append("*")
+#         l2.append(l1)
+#     l1.append(l2)
+
+# print(l3)
+
+
+# 13. Write a Python program to print the numbers of a specified list after removing even numbers from it. 
+
+
+# 14. Write a python program to print the list in square of the list elements.
+
+# l = [[1,2,3,4],[5,6,7,8],[9,7,5,4]]
+# l1 = []
+# for i in l:
+#     l2 = []
+#     for j in i:
+#         l2.append(j**2)
+#     l1.append(l2)
+
+# print(l1)
+
