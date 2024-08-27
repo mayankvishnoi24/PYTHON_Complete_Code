105863c7802f9b34140c065203739abc6db12589
Mayank Vishnoi <134063120+mayankvishnoi24@users.noreply.github.com>
2024-08-27 16:44:31 +0530

    Updated

---
+++ 07.List_in_python/01.first.py
@@ -0,0 +1,27 @@
+### List in Python
+
+# l1 = [1,2,3,4,5]
+# l2 = ['Mayank','Rohit','kirti','Aashish'] ## Multiple elements are adding
+
+# for i in range(6):
+#     l2.append(input("enter the element:"))
+# print(l2)
+
+## Check the element is in the list 
+# if 5 in l1:
+#     print("Yes")
+# else:
+#     print("No")
+
+## Slicing in list
+l1 = [1,2,3,4,5]
+# l2 = ['Mayank','Rohit','kirti','Aashish','Rajesh']
+# print(l2[0::2])
+
+l2 = [1,2,5,2+3j,True,"Raj"]
+print(l2)
+# Type Casting in List
+print(tuple(l2))
+print(set(l2))
+
+print(type(l2))
